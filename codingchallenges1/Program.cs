ac30c12ae094acad33a44aca1df29b045b9e9e8a
alicia984@revature.net <alicia984@revature.net>
2022-07-25 12:31:12 -0500

    On branch codingchallenges1  Changes to be committed: 	new file:   codingChallenges/1_Strings/.vscode/launch.json 	new file:   codingChallenges/1_Strings/.vscode/tasks.json 	new file:   codingChallenges/1_Strings/1_Strings/.vscode/launch.json 	new file:   codingChallenges/1_Strings/1_Strings/.vscode/tasks.json 	new file:   codingChallenges/1_Strings/1_Strings/Strings/README.txt 	new file:   codingChallenges/1_Strings/1_Strings/Strings/Strings.cs 	new file:   codingChallenges/1_Strings/1_Strings/Strings/Strings.csproj 	new file:   codingChallenges/1_Strings/1_Strings/Strings/Strings.sln 	new file:   codingChallenges/1_Strings/1_Strings/Tests.Strings/Tests.Strings.csproj 	new file:   codingChallenges/1_Strings/1_Strings/Tests.Strings/UnitTest1.cs 	new file:   codingchallenges1/Program.cs 	new file:   codingchallenges1/codingchallenges.csproj

---
+++ codingchallenges1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace codingchallenges
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter a string.");
+            string userInput1 = Console.ReadLine();
+            string stringToUpperResult = StringToUpper(userInput1);
+            Console.WriteLine(stringToUpperResult);
+
+            Console.WriteLine("Please, enter a string.");
+            string userInput2 = Console.ReadLine();
+            string stringToLowerResult = StringToUpper(userInput2);
+            Console.WriteLine(stringToLowerResult);
+        }
+       // <summary>
+        //This method has one string parameter and will
+        //1) change the stringto all upper case and
+        //2) return the new string
+        //</summary>
+        //<param name="userString"></ParamArrayAttribute>
+        //<returns></returns>
+        public static string StringToUpper(string userString)
+        {
+            return userString.ToUpper();
+        }
+
+        //This method has one string parameter and will
+        //1)change the string to all lower case and
+        //2) return the new string
+        //</summary>
+        //<param name="userString"></ParamArrayAttribute>
+        //<returns></returns>
+        public static string StringToLower(string userString)
+        {
+            return userString.ToLower();
+        }
+    }
+}
