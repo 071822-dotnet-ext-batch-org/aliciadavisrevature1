162352cf82e10b6154a74899144092160a4aa734
alicia984@revature.net <alicia984@revature.net>
2022-08-09 16:51:35 -0500

    adding group console app for http client project

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+
+namespace ChuckNorris
+
+{
+
+    class Program2
+    {
+    static void Main(string[] args)
+        {
+            using(HttpClient? source = new HttpClient())//Send HTTP requests and receive HTTP responses
+                {   
+                    Uri? endpoint = new Uri("http://api.icndb.com/jokes/random?firstName=Ringo&lastName=Starr"); //Source url changes Chuck Norris to Ringo Starr
+
+                    HttpResponseMessage? result = source.GetAsync(endpoint).Result; //HTTP response messasge and status
+            
+                    string? json = result.Content.ReadAsStringAsync().Result; //Convert result to JSON   
+                
+                    Console.WriteLine(json); //Output the JSON
+                }
+        }//EoM
+    }//EoC
+
+}//EoN
+
